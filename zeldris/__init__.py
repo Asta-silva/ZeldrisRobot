090c5b05879a1f3a7551e1a40c2eaeb3cbcd9065
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-20 22:13:20 +0530

    Update __init__.py

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -106,7 +106,7 @@ if ENV:
     PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
 
-    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = os.environ.get("DATABASE_URL", " postgres://qhazacag:YdDPbUTz722YQNqpbCqjUdT927WFY4dC@fanny.db.elephantsql.com/qhazacag")
     REDIS_URL = os.environ.get("REDIS_URL")
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()

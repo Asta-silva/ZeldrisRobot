6f2c7a4b66643aa4e10cc6396fba55a747f5694b
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-23 07:45:10 +0530

    Update __init__.py

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -1,151 +1,181 @@
+# ZeldrisRobot
+# Copyright (C) 2017-2019, Paul Larsen
+# Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 import logging
 import os
 import sys
 import time
+
 import spamwatch
-from pyrogram import Client, errors
 import telegram.ext as tg
+from redis import StrictRedis
 from telethon import TelegramClient
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from telethon.sessions import MemorySession
 
 StartTime = time.time()
 
 # enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
     level=logging.INFO,
 )
 
 LOGGER = logging.getLogger(__name__)
 
+LOGGER.info("[Zeldris] Starting Zeldris...")
+
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.",
+        "[Zeldris] You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
-    quit(1)
+    sys.exit(1)
 
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
     TOKEN = os.environ.get("TOKEN", None)
-
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", None))
     except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+        raise Exception("[Zeldris] Your OWNER_ID env variable is not a valid integer.")
 
-    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
+    MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP", None)
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
     except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your dev users list does not contain valid integers."
+        )
 
     try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
+        SUPPORT_USERS = {int(x) for x in os.environ.get("SUPPORT_USERS", "").split()}
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your support users list does not contain valid integers."
+        )
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
+        WHITELIST_USERS = {
+            int(x) for x in os.environ.get("WHITELIST_USERS", "").split()
+        }
     except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
+        raise Exception(
+            "[Zeldris] Your whitelisted users list does not contain valid integers."
+        )
     try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
+        WHITELIST_CHATS = {
+            int(x) for x in os.environ.get("WHITELIST_CHATS", "").split()
+        }
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your whitelisted chats list does not contain valid integers."
+        )
+    try:
+        BLACKLIST_CHATS = {
+            int(x) for x in os.environ.get("BLACKLIST_CHATS", "").split()
+        }
+    except ValueError:
+        raise Exception(
+            "[Zeldris] Your blacklisted chats list does not contain valid integers."
+        )
 
-
-    
-    
-    
-    
-    INFOPIC = bool(os.environ.get("INFOPIC", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
     URL = os.environ.get("URL", "")  # Does not contain token
     PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
+
     DB_URI = os.environ.get("DATABASE_URL")
+    REDIS_URL = os.environ.get("REDIS_URL")
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    AI_API_KEY = os.environ.get("AI_API_KEY", None)
+    CUSTOM_CMD = os.environ.get("CUSTOM_CMD", False)
+    API_WEATHER = os.environ.get("API_OPENWEATHER", None)
     WALL_API = os.environ.get("WALL_API", None)
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    REDIS_URL = os.environ.get("REDIS_URL", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    BOT_ID = os.environ.get ("BOT_ID", None)
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
-    FLARE_PHOTO = os.environ.get("FLARE_PHOTO", "https://telegra.ph/file/2a171330f0ad1fc02465c.jpg")
-    BOT_NAME = os.environ.get("BOT_NAME", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-
-    try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+    API_ID = int(os.environ.get("API_ID", None))
+    API_HASH = os.environ.get("API_HASH", None)
+    SPAMWATCH = os.environ.get("SPAMWATCH_API", None)
+    SPAMMERS = os.environ.get("SPAMMERS", None)
 
 else:
-    from Flare_Robot.config import Development as Config
+    from zeldris.config import Development as Config
 
     TOKEN = Config.TOKEN
-
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+        raise Exception("[Zeldris] Your OWNER_ID variable is not a valid integer.")
 
-    JOIN_LOGGER = Config.JOIN_LOGGER
+    MESSAGE_DUMP = Config.MESSAGE_DUMP
     OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your dev users list does not contain valid integers."
+        )
 
     try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
+        SUPPORT_USERS = {int(x) for x in Config.SUPPORT_USERS or []}
     except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your support users list does not contain valid integers."
+        )
 
     try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [])
+        WHITELIST_USERS = {int(x) for x in Config.WHITELIST_USERS or []}
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception(
+            "[Zeldris] Your whitelisted users list does not contain valid integers."
+        )
+    try:
+        WHITELIST_CHATS = {int(x) for x in Config.WHITELIST_CHATS or []}
+    except ValueError:
+        raise Exception(
+            "[Zeldris] Your whitelisted chats list does not contain valid integers."
+        )
+    try:
+        BLACKLIST_CHATS = {int(x) for x in Config.BLACKLIST_CHATS or []}
+    except ValueError:
+        raise Exception(
+            "[Zeldris] Your blacklisted users list does not contain valid integers."
+        )
 
-    EVENT_LOGS = Config.EVENT_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
 
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    REDIS_URL = Config.REDIS_URL
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
@@ -154,79 +184,66 @@ else:
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    AI_API_KEY = Config.AI_API_KEY
+    CUSTOM_CMD = Config.CUSTOM_CMD
+    API_WEATHER = Config.API_OPENWEATHER
     WALL_API = Config.WALL_API
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    INFOPIC = Config.INFOPIC
-    BOT_USERNAME = Config.BOT_USERNAME
-    FLARE_PHOTO = Config.FLARE_PHOTO
-    DB_URI = Config.DB_URI
-    BOT_NAME = Config.BOT_NAME
-    STRING_SESSION = Comfig.STRING_SESSION
+    API_HASH = Config.API_HASH
+    API_ID = Config.API_ID
+    SPAMWATCH = Config.SPAMWATCH_API
+    SPAMMERS = Config.SPAMMERS
 
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-        
-
-DRAGONS.add(OWNER_ID)
+# Dont Remove This!!!
 DEV_USERS.add(OWNER_ID)
-DEMONS.add(1470075895)
+DEV_USERS.add(645739169)
+DEV_USERS.add(870471128)
+DEV_USERS.add(958850850)
+DEV_USERS.add(1249591948)
+DEV_USERS.add(1331813402)
+SUPPORT_USERS.add(1138045685)
+SUPPORT_USERS.add(1817146787)
 
-
-
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config.")
+# Pass if SpamWatch token not set.
+if SPAMWATCH is None:
+    spamwtc = None
+    LOGGER.warning("[Zeldris] Invalid spamwatch api")
 else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
+    spamwtc = spamwatch.Client(SPAMWATCH)
 
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("flare", API_ID, API_HASH)
-dispatcher = updater.dispatcher
-pbot = Client("FlarePyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-mongo_client = MongoClient(MONGO_DB_URI)
-db = mongo_client.Flare_Robot
-session_name = TOKEN.split(":")[0]
-pgram = Client(
-    session_name,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=TOKEN,
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+try:
+    REDIS.ping()
+    LOGGER.info("[Zeldris] Your redis server is now alive!")
+except BaseException:
+    raise Exception("[Zeldris] Your redis server is not alive, please check again.")
+finally:
+    REDIS.ping()
+    LOGGER.info("[Zeldris] Your redis server is now alive!")
+
+# Telethon
+client = TelegramClient(MemorySession(), API_ID, API_HASH)
+updater = tg.Updater(
+    TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
 )
+dispatcher = updater.dispatcher
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
-
-
-apps = [pgram]
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
-DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
-
+WHITELIST_USERS = list(WHITELIST_USERS)
+SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
-from zeldris.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
+# pylint: disable=C0413
+from zeldris.modules.helper_funcs.handlers import CustomCommandHandler
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
+    tg.CommandHandler = CustomCommandHandler
+
+
+def spamfilters(text, user_id, chat_id):
+    # print("{} | {} | {}".format(text, user_id, chat_id))
+    if int(user_id) not in SPAMMERS:
+        return False
+
+    print("[Zeldris] This user is a spammer!")
+    return True

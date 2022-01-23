2fab0ac615615e3a80c62db281b283259d5ffd0b
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-23 07:35:19 +0530

    Update __main__.py

--- zeldris/__main__.py
+++ zeldris/__main__.py
@@ -95,13 +95,9 @@ ZELDRIS_IMG = "https://telegra.ph/file/1fa00785f30375c0c1b50.jpg"
 
 PM_START_TEXT = """
 Hey there! my name is *{}*. 
-A modular group management bot with useful features. [ㅤ](https://telegra.ph/file/fed9ba09e9add9b197c21.png)
+A modular group management bot with useful features. [ㅤ](https://telegra.ph/file/4d339f72dfe6e545efac5.jpg)
 
 ◑ *Uptime:* `{}`
-◑ `{}` *Users, across* `{}` *chats.*
-
-Any issues or need help related to me?
-Join our official group [IDNCoderX](https://t.me/IDNCoderX).
 Click help button to know my commands!
 """
 
@@ -113,13 +109,13 @@ buttons = [
         ),
         InlineKeyboardButton(
             text="Updates 📢",
-            url="https://t.me/IDNCoder",
+            url="https://t.me/Freia_Updates",
         ),
     ],
     [
         InlineKeyboardButton(
-            text="Add Zeldris to Your Group 👥",
-            url="t.me/ZeldrisRobot?startgroup=true",
+            text="Add Flare to Your Group 👥",
+            url="t.me/Flare_Robot?startgroup=true",
         ),
     ],
 ]
@@ -265,11 +261,11 @@ def start(update: Update, context: CallbackContext):
                     [
                         InlineKeyboardButton(
                             text="☎️ Support",
-                            url="https://t.me/IDNCoderX",
+                            url="https://t.me/Freia_Support",
                         ),
                         InlineKeyboardButton(
                             text="Updates 📡",
-                            url="https://t.me/IDNCoder",
+                            url="https://t.me/Freia_Updates",
                         ),
                     ]
                 ]

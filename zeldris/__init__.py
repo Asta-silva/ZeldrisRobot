4f1986b2f76ad82b1c3e206ac3111265c3181c61
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-23 08:17:54 +0530

    Update __init__.py

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -222,6 +222,7 @@ finally:
 # Telethon
 client = TelegramClient(MemorySession(), API_ID, API_HASH)
 telethn = TelegramClient("flare", API_ID, API_HASH)
+pbot = Client("FlarePyro", bot_token=TOKEN)
 updater = tg.Updater(
     TOKEN,
     workers=min(32, os.cpu_count() + 4),
